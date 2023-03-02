06b012dae9128e3f2d2123a438724d47e87f3873
nekithar10 <126747569+nekithar10@users.noreply.github.com>
2023-03-01 23:15:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('Breakfast')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
